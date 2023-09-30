b1e6c17a98e0d1b7a5af5100f38b159d5c14d011
CesarPaz-cloud <cesarpazfalcon1@gmail.com>
2023-09-30 00:49:04 -0500

    Commit Inicial

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL1REST_9/DswiiCl1Rest9Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL1REST_9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1Rest9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl1Rest9Application.class, args);
+	}
+
+}
